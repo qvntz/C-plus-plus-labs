0667968e0411f8b519d803551636578dd4084c83
qvntz <asdawd2004@mail.ru>
2021-04-03 21:09:43 +0300

    added an example of iterators

---
+++ iterators/ex1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+
+template <typename T>
+vector<T> FindGreaterElements(const set<T>& elements, const T& border) {
+	auto it = find_if(begin(elements), end(elements), [&](const T& elem) { return elem > border; });
+	
+	return { it, end(elements) };
+}
+
+int main() {
+	for (int x : FindGreaterElements(set<int>{1, 5, 7, 8}, 5)) {
+		cout << x << " ";
+	}
+	cout << endl;
+
+	string to_find = "Python";
+	cout << FindGreaterElements(set<string>{"C", "C++"}, to_find).size() << endl;
+	return 0;
+}
